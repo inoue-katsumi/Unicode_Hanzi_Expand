422079aaf9eafca8b7203e8f5ab3aa26c251e1d7
INOUE,Katsumi <INOUE.Katsumi@local>
2019-11-20 11:29:18 +0900

    Fixed uhgrep.sh.

--- uhgrep.sh
+++ uhgrep.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+# grep wrapper to find kanji variants. 
+# 1st arg should be filter string. No 'grep' option is allowed.
+# May not work with a lot of regex special chars.
+#set -x
 uhgrep () 
 { 
     expanded="";
     for ((i=0; i<${#1}; i++))
     do
-        expanded=${expanded}$(awk "/^${1:i:1}/ {printf(\"%s\",\$2)}" ~/unihan.tsv);
+        expanded=${expanded}$(
+	awk " \
+	  /^\\${1:i:1}/ {printf(\"%s\",\$2);found=1;exit} \
+          END         {if(!found) printf(\"%s\",\"${1:i:1}\")} \
+	" ~/unihan.tsv
+	);
     done;
     shift;
     grep -i --color=auto -E $expanded $*
